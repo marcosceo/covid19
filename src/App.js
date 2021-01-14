a636cc005ef09ccc2c6a1bc6fb2bb83aae1ad652
Marcos <marcos.ceo@outlook.com>
2021-01-14 20:37:07 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+import { StylesProvider } from '@material-ui/styles'
+import { CssBaseline } from '@material-ui/core'
+import  GlobalStyle  from './commons/styles/global-style'
+import Main from './container/Main'
 
 function App() {
   return (
-   <h1>Covid 19</h1>
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyle />
+     <Main />
+    </StylesProvider>
+    
   );
 }
 
